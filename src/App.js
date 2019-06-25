6f34abbff9e85a3e8c65389c79c4a8070aba07da
Kim <kim.hupponen@abo.fi>
2019-06-25 19:07:57 +0300

    Dashboard has the datepicker and a list of nodes not working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import { Admin, Resource  } from 'react-admin';
-import Calendar from './Dashboard'
+
+import {Dashboard} from './Dashboard'
 import jsonServerProvider from 'ra-data-json-server';
-import restProvider from 'ra-data-simple-rest';
-import {DemandList, DemandShow} from "./Demand";
-
-const dataProvider = jsonServerProvider('https://my-json-server.typicode.com/msand/demo');
+import {Admin, Resource, ListGuesser} from "react-admin";
 const App = () => (
-    <Admin dashboard={Calendar} dataProvider={jsonServerProvider(  'https://my-json-server.typicode.com/msand/demo')}>
-        <Resource
-            GET_ONE
-            name={"nodes"}
-            list={DemandList}
-            show={DemandShow}
-
-        />
+    <Admin dashboard={Dashboard} dataProvider={jsonServerProvider('https://my-json-server.typicode.com/msand/demo')}>
+        <Resource  name="nodes" list={ListGuesser}/>
     </Admin>
     );
 
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,54 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import moment from "moment";
+import Calendar from "./DateRangePicker"
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import {Title} from 'react-admin';
 
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import { DateRangePicker } from 'react-dates';
+export const Dashboard = (props) => (
+    <div>
+        <Title title="Date Picker" />
+        <CardContent>
+            <Calendar/>
+        </CardContent>
+    </div>
+);
 
-
-
-export class Calendar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            startDate: null,
-            endDate: null,
-            focusedInput: null,
-        };
-
-    }
-    /*componentDidMount() {
-        fetch('https://my-json-server.typicode.com/msand/demo/nodes')
-            .then(response => response.json())
-            .then((jsonData) => {
-                this.setState({
-                    startDate: moment(JSON.parse(JSON.stringify(jsonData[0])).timePeriod.start.value),
-                    endDate: moment(JSON.parse(JSON.stringify(jsonData[0])).timePeriod.end.value)
-                })
-            })
-            .catch((error) => {
-                console.error(error)
-            });
-   }
-   */
-        render() {
-
-        return (
-
-            <div className="Calendar">
-                <DateRangePicker
-                    startDateId="startDate"
-                    endDateId="endDate"
-                    startDate={this.state.startDate}
-                    endDate={this.state.endDate}
-                    onDatesChange={({ startDate, endDate }) => { this.setState({ startDate, endDate })}}
-                    focusedInput={this.state.focusedInput}
-                    onFocusChange={(focusedInput) => { this.setState({ focusedInput })}}
-                />
-            </div>
-        );
-    }
-}
-
-export default Calendar;
+export default Dashboard;
