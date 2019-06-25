c0aaab307799060049731ce434cedd064a8fcadf
Kim <kim.hupponen@abo.fi>
2019-06-25 19:26:34 +0300

    Updates

---
+++ precache-manifest.ee09862b0cec8ff92955d5862ff0fd43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e2cce613d52f0fe8b13f883a6aa66a",
+    "url": "/calendar-example/index.html"
+  },
+  {
+    "revision": "d466b40b393ab3de62b0",
+    "url": "/calendar-example/static/css/2.6b000025.chunk.css"
+  },
+  {
+    "revision": "aebdef70dbe6c7ec0fd1",
+    "url": "/calendar-example/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "d466b40b393ab3de62b0",
+    "url": "/calendar-example/static/js/2.320640dd.chunk.js"
+  },
+  {
+    "revision": "aebdef70dbe6c7ec0fd1",
+    "url": "/calendar-example/static/js/main.251b131d.chunk.js"
+  },
+  {
+    "revision": "c37995487641caff1a7d",
+    "url": "/calendar-example/static/js/runtime~main.12831df2.js"
+  }
+]);
